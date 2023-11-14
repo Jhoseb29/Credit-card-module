e1ccc6dbac6e8f304024798eb5c3abb54b40e214
Carlos Sejas <Carlos.Sejas@jala.university>
2023-11-14 09:03:44 -0500

    US12/GeneracionBinDeTarjeta/ muestra el bin y cvv de la tarjeta

--- src/main/java/org/jala/university/utilities/CreditCardUtilities.java
+++ src/main/java/org/jala/university/utilities/CreditCardUtilities.java
@@ -6,9 +6,17 @@ public class CreditCardUtilities {
     static Random random = new Random();
     public static String generateCreditCardBin(){
         StringBuilder cardBin = new StringBuilder("4");
-        for(int i = 1; i <= 15; i++){
+        for(int i = 1; i <= 3; i++){
             int digit = random.nextInt(10);
             cardBin.append(digit);
+        }
+        cardBin.append(" ");
+        for(int i = 1; i <= 12; i++){
+            int digit = random.nextInt(10);
+            cardBin.append(digit);
+            if (i % 4 == 0) {
+                cardBin.append(" ");
+            }
         }
         return cardBin.toString();
     }
