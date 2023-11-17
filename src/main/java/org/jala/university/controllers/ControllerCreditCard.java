82e1583d7cb454b0ff6d9905a19b2da05136928b
daleth vasquez <daleth.vasquez@jala.university>
2023-11-17 03:23:33 +0000

    Us10/retiro de efectivo/dv

--- src/main/java/org/jala/university/controllers/ControllerCreditCard.java
+++ src/main/java/org/jala/university/controllers/ControllerCreditCard.java
@@ -1,33 +1,28 @@
 package org.jala.university.controllers;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
-import org.jala.university.dao.RecordDao;
 import org.jala.university.model.CreditCardModel;
 import org.jala.university.model.RecordModel;
 import org.jala.university.services.CreditCardModule;
 import org.jala.university.services.RecordImpl;
 import org.jala.university.utilities.EnumOperations;
+import org.jala.university.utilities.Validator;
 
 import java.time.LocalDateTime;
 import java.util.Date;
-import java.util.UUID;
 
 
-public class ControllerRecordCard {
+public class ControllerCreditCard {
     private final CreditCardModel creditCardModel;
     private final EntityManager entityManager;
     private final   CreditCardModule creditCardModule;
     private final RecordImpl record;
 
-    public ControllerRecordCard(CreditCardModel creditCardModel, EntityManager entityManager, CreditCardModule creditCardModule, RecordImpl record ) {
+    public ControllerCreditCard(CreditCardModel creditCardModel, EntityManager entityManager, CreditCardModule creditCardModule, RecordImpl record ) {
         this.creditCardModel = creditCardModel;
         this.entityManager = entityManager;
         this.creditCardModule = creditCardModule;
         this.record = record;
-    }
-    public String getTypeOfOperation(EnumOperations operation) {
-       return operation.getOperationsName();
-
     }
     public void updatePin(int pin) {
         creditCardModel.setNIP(pin);
@@ -45,13 +40,32 @@ public class ControllerRecordCard {
         int currentBalance = (int) creditCardModel.getCurrent_limit();
         currentBalance -= mount;
         creditCardModel.setCurrent_limit(currentBalance);
-        logAction(EnumOperations.PAY.getOperationsName());
+        logAction(EnumOperations.PAY.getOperationsName() + currentBalance);
         creditCardModule.update(creditCardModel);
         commitTransaction();
         return currentBalance;
 
     }
 
+    public int withdrawCash(int mount) {
+        int currentBalance = (int) creditCardModel.getCurrent_limit();
+        int statusCard = creditCardModel.getStatus();
+        if (!Validator.isValidWithdrawal(mount, currentBalance, statusCard)) {
+            System.out.println("Invalid withdrawal amount. Please check your balance or review the status of the card");
+            return currentBalance;
+        }
+        int desc = (int) (mount * 0.05);
+        if (desc >= currentBalance) {
+            System.out.println("Insufficient funds. Please check your balance.");
+            return currentBalance;
+        }
+        currentBalance -= mount + desc;
+        creditCardModel.setCurrent_limit(currentBalance);
+        logAction(EnumOperations.WITHDRAW_CASH.getOperationsName() + mount);
+        creditCardModule.update(creditCardModel);
+        commitTransaction();
+        return currentBalance;
+    }
     private void logAction(String action){
         String logMessage = String.format("Action: %s, Date: %s", action, LocalDateTime.now());
         EntityTransaction transaction = entityManager.getTransaction();
@@ -67,6 +81,7 @@ public class ControllerRecordCard {
         record.create(logRecord);
         commitTransaction();
     }
+
     private void commitTransaction() {
         EntityTransaction transaction = entityManager.getTransaction();
         try {
@@ -81,5 +96,4 @@ public class ControllerRecordCard {
             e.printStackTrace();
         }
     }
-
 }
