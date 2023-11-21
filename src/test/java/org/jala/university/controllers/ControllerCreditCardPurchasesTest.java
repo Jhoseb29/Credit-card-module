8074b6f3fffbaf8ca6571024af92bec4389f7c66
AbelMancilla <abel.mancilla@jala.university>
2023-11-20 23:44:03 -0400

    test/US08

---
+++ src/test/java/org/jala/university/controllers/ControllerCreditCardPurchasesTest.java
@@ -0,0 +1,51 @@
+package org.jala.university.controllers;
+
+import org.jala.university.model.CreditCardModel;
+import org.jala.university.services.CreditCardModule;
+import org.jala.university.services.RecordImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+
+public class ControllerCreditCardPurchasesTest {
+
+  CreditCardModel creditCardModel;
+  ControllerCreditCard controllerCreditCard;
+  CreditCardModule creditCardModule;
+  RecordImpl record;
+
+  @BeforeEach
+  public void setup() {
+    creditCardModel = CreditCardModel.builder().build();
+    creditCardModule = mock(CreditCardModule.class);
+    record = mock(RecordImpl.class);
+    controllerCreditCard = new ControllerCreditCard(creditCardModel, null, creditCardModule,
+        record);
+  }
+
+  /*@Test
+  void payTestSufficientFunds() {
+    int initialBalance = 1000;
+    int paymentAmount = 500;
+
+    creditCardModel.setCurrent_limit(initialBalance);
+
+    int newBalance = controllerCreditCard.pay(paymentAmount);
+
+    int expectedBalance = initialBalance - paymentAmount;
+    assertEquals(expectedBalance, newBalance);
+  }*/
+
+  @Test
+  void payTestInsufficientFunds() {
+    int initialBalance = 100;
+    int paymentAmount = 500;
+
+    creditCardModel.setCurrent_limit(initialBalance);
+    int newBalance = controllerCreditCard.pay(paymentAmount);
+    assertEquals(initialBalance, newBalance);
+  }
+}
