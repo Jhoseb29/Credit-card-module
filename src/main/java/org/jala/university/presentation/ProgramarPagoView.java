6a456126b0a1669a516a979d1f8d7bb9bcac019e
Jhoseb <jose.moya@jala.university>
2023-11-21 15:00:13 -0500

    US-009 Fix JDateChoor chage for JcomboBox

--- src/main/java/org/jala/university/presentation/ProgramarPagoView.java
+++ src/main/java/org/jala/university/presentation/ProgramarPagoView.java
@@ -1,19 +1,19 @@
 package org.jala.university.presentation;
 
-
 import org.jala.university.controllers.ControllerCreditCard;
 
 import javax.swing.*;
 import java.awt.*;
+import java.time.DateTimeException;
 import java.time.LocalDate;
-import java.time.ZoneId;
-//import com.toedter.calendar.JDateChooser;
-
+import java.util.stream.IntStream;
 
 public class ProgramarPagoView extends JDialog {
     private final ControllerCreditCard controllerCreditCard;
 
-    //private JDateChooser dateChooser;
+    private JComboBox<Integer> dayComboBox;
+    private JComboBox<Integer> monthComboBox;
+    private JComboBox<Integer> yearComboBox;
     private JTextField montoField;
 
     public ProgramarPagoView(ControllerCreditCard controllerCreditCard) {
@@ -27,15 +27,21 @@ public class ProgramarPagoView extends JDialog {
         setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        //dateChooser = new JDateChooser();
+        dayComboBox = new JComboBox<>(IntStream.rangeClosed(1, 31).boxed().toArray(Integer[]::new));
+        monthComboBox = new JComboBox<>(IntStream.rangeClosed(1, 12).boxed().toArray(Integer[]::new));
+        yearComboBox = new JComboBox<>(IntStream.rangeClosed(LocalDate.now().getYear(), LocalDate.now().getYear() + 10).boxed().toArray(Integer[]::new));
         montoField = new JTextField();
 
         JButton confirmButton = new JButton("Confirmar");
         JButton cancelButton = new JButton("Cancelar");
 
-        JPanel panel = new JPanel(new GridLayout(3, 2));
-        panel.add(new JLabel("Fecha de Pago:"));
-        //panel.add(dateChooser);
+        JPanel panel = new JPanel(new GridLayout(4, 2));
+        panel.add(new JLabel("Día de Pago:"));
+        panel.add(dayComboBox);
+        panel.add(new JLabel("Mes de Pago:"));
+        panel.add(monthComboBox);
+        panel.add(new JLabel("Año de Pago:"));
+        panel.add(yearComboBox);
         panel.add(new JLabel("Monto:"));
         panel.add(montoField);
         panel.add(confirmButton);
@@ -49,13 +55,16 @@ public class ProgramarPagoView extends JDialog {
 
     private void confirmarProgramacion() {
         try {
-            LocalDate fechaPago = dateChooser.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            int day = (int) dayComboBox.getSelectedItem();
+            int month = (int) monthComboBox.getSelectedItem();
+            int year = (int) yearComboBox.getSelectedItem();
+
+            LocalDate fechaPago = LocalDate.of(year, month, day);
             int montoPago = Integer.parseInt(montoField.getText());
 
             controllerCreditCard.programarPagoAutomatico(fechaPago, montoPago);
-            JOptionPane.showMessageDialog(this, "Pago programado correctamente.");
             dispose();
-        } catch (NumberFormatException | NullPointerException ex) {
+        } catch (NumberFormatException | NullPointerException | DateTimeException ex) {
             JOptionPane.showMessageDialog(this, "Ingrese una fecha y un monto válido.", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
