5e67f6d977e6ad1172da80a8b9abfe78695cafc7
AbelMancilla <abel.mancilla@jala.university>
2023-11-24 01:06:31 -0400

    US13/PruebasUnitarias/DV se realizo pruebas unitarias de los metodos usados en el Formulario

--- src/main/java/org/jala/university/services/FormImpl.java
+++ src/main/java/org/jala/university/services/FormImpl.java
@@ -15,8 +15,9 @@ public class FormImpl implements FormModule {
     }
 
     @Override
-    public void create(FormModel creditCard) {
-        formDao.create(creditCard);
+    public FormModel create(FormModel formModel) {
+        formDao.create(formModel);
+        return formModel;
     }
 
     @Override
@@ -35,7 +36,7 @@ public class FormImpl implements FormModule {
     }
 
     @Override
-    public void delete(UUID id) {
+    public void deleteById(UUID id) {
         formDao.deleteById(id);
     }
 
