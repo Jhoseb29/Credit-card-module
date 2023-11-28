343b538338e0c91a9b374291427bc5c03f3a5deb
Jhoseb <jose.moya@jala.university>
2023-11-27 21:03:35 -0500

    Preparando para la version 1.1.1

--- src/main/java/org/jala/university/main.java
+++ src/main/java/org/jala/university/main.java
@@ -2,18 +2,9 @@ package org.jala.university;
 
 
 import jakarta.persistence.*;
-import org.jala.university.dao.CreditCardDao;
-import org.jala.university.model.CreditCardModel;
 import org.jala.university.presentation.InterfazView;
-import org.jala.university.services.CreditCardImpl;
-import org.jala.university.services.CreditCardModule;
-import org.jala.university.utilities.CreditCardUtilities;
-
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.UUID;
 
-import static org.jala.university.utilities.Querys.validateCreditCard;
 
 public class main {
     private static EntityManager entityManager;
